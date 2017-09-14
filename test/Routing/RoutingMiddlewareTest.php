a173a9feb9201153c8bd375d79bc4746f80e30ed
Yaroslav Kharchenko <undertext.2010@gmail.com>
2017-09-14 21:18:33 +0300

    Adding test for RoutingMiddleware

---
+++ test/Routing/RoutingMiddlewareTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace ARouter\Routing;
+
+use Interop\Http\ServerMiddleware\DelegateInterface;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+/**
+ * Tests RoutingMiddleware functionality.
+ */
+class RoutingMiddlewareTest extends TestCase {
+
+  /**
+   * Tests process method.
+   */
+  public function testRoutingMiddleware() {
+    $request = $this->createMock(ServerRequestInterface::class);
+    $response = $this->createMock(ResponseInterface::class);
+    $delegate = $this->createMock(DelegateInterface::class);
+    $routeHandler = $this->createMock(RouteHandler::class);
+    $routeHandler->method('execute')->willReturn($response);
+    $router = $this->createMock(Router::class);
+    $router->method('getRouteHandler')->willReturn($routeHandler, NULL);
+    $routingMiddleware = new RoutingMiddleware($router);
+
+    $result = $routingMiddleware->process($request, $delegate);
+    self::assertEquals($result, $response);
+    $result = $routingMiddleware->process($request, $delegate);
+    self::assertNull($result);
+  }
+
+}
