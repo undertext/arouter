a1f76247f43bfca18f77e7f5ea3eec35acda2c4f
Yaroslav Kharchenko <undertext.2010@gmail.com>
2017-11-01 23:12:23 +0200

    Adding UrlBuilder.

---
+++ src/Routing/UrlBuilder.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace ARouter\Routing;
+
+use ARouter\Routing\Annotation\Route;
+use Doctrine\Common\Annotations\AnnotationReader;
+
+/**
+ * Allows generating of URLs in a nice way.
+ */
+class UrlBuilder {
+
+  /**
+   * Doctrine Annotations reader.
+   *
+   * @var \Doctrine\Common\Annotations\AnnotationReader
+   */
+  private $annotationReader;
+
+  /**
+   * UrlBuilder constructor.
+   */
+  public function __construct() {
+    $this->annotationReader = new AnnotationReader();
+  }
+
+  /**
+   * Generate URL from path pattern.
+   *
+   * @param string $path
+   *   Path pattern.
+   * @param array $args
+   *   Path arguments.
+   *
+   * @return string
+   *   Generated URL.
+   *
+   * @throws \Exception
+   *   If path argument is missing for URL generation.
+   */
+  public function fromPath(string $path, array $args): string {
+    preg_match_all('|{([^}]*)}|', $path, $matches);
+    foreach ($matches[1] as $match) {
+      if (!isset($args[$match])) {
+        throw new \Exception("Path argument '{$match}' is missing for URL generation from path '{$path}'");
+      }
+      $path = str_replace('{' . $match . '}', $args[$match], $path);
+    }
+    return $path;
+  }
+
+  /**
+   * Generate URL from controller and method name.
+   *
+   * @param string $controllerClass
+   *   Name of controller class.
+   * @param string $method
+   *   Name of controller's method.
+   * @param array $args
+   *   Path arguments.
+   *
+   * @return string
+   *   Generated URL.
+   */
+  public function fromControllerMethod(string $controllerClass, string $method, array $args) {
+    $annotationReader = new AnnotationReader();
+    $reflectionMethod = new \ReflectionMethod($controllerClass, $method);
+    $routeAnnotation = $annotationReader->getMethodAnnotation($reflectionMethod, Route::class);
+    return $this->fromPath($routeAnnotation->path, $args);
+  }
+}
---
+++ test/Routing/UrlBuilderTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace ARouter\Routing;
+
+use PHPUnit\Framework\TestCase;
+use ARouter\Routing\Controllers\SubDirectory\TestController2;
+
+/**
+ * Tests RoutingMiddleware functionality.
+ */
+class UrlBuilderTest extends TestCase {
+
+  /**
+   * URL builder.
+   *
+   * @var \ARouter\Routing\UrlBuilder
+   */
+  private $urlBuilder;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    $this->urlBuilder = new UrlBuilder();
+  }
+
+  /**
+   * Tests URL generation from path.
+   */
+  public function testFromPath() {
+    $url = $this->urlBuilder->fromPath('user/{name}/{action}', [
+      'name' => 'username',
+      'action' => 'view'
+    ]);
+    self::assertEquals($url, 'user/username/view');
+  }
+
+  /**
+   * Tests exception while URL generation from path.
+   */
+  public function testExceptionFromPath() {
+    $this->expectException(\Exception::class);
+    $this->urlBuilder->fromPath('user/{name}/{action}', [
+      'name' => 'username',
+    ]);
+  }
+
+  /**
+   * Tests URL generation from controller and method name.
+   */
+  public function testFromControllerMethod() {
+    $url = $this->urlBuilder->fromControllerMethod(TestController2::class, 'action2', ['action' => 'view']);
+    self::assertEquals($url, 'testpath2/view');
+  }
+
+}
