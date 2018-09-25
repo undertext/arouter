0222ea6a66bceb3e13e6dae918a1b3ddc9152e83
Yoroslav Kharchenko <yaroslav.kharchenko@mediamonks.net>
2018-09-25 18:31:23 +0200

    Decrypt key correctly

--- .ci/setup-ssh.sh
+++ .ci/setup-ssh.sh
@@ -5,11 +5,10 @@ set -ev
 declare -r SSH_FILE="$(mktemp -u $HOME/.ssh/travis_temp_ssh_key_XXXX)"
 # Decrypt the file containing the private key (put the real name of the variables)
 openssl aes-256-cbc \
-  -K $encrypted_6e055bfe150f_key \
-  -iv $encrypted_6e055bfe150f_iv \
+  -K $encrypted_91a194f3ab43_key \
+  -iv $encrypted_91a194f3ab43_iv \
   -in ".ci/travis_rsa.enc" \
   -out "$SSH_FILE" -d
-cat $SSH_FILE
 # Enable SSH authentication
 chmod 600 "$SSH_FILE" \
   && printf "%s\n" \
